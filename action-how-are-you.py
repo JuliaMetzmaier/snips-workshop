6158a99f816d7a4830a7e3288f092436f973189b
JulesMitzy <julina.maier@googlemail.com>
2019-05-08 16:01:11 +0200

    win

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -40,7 +40,7 @@ def how_are_you_callback(hermes, intent_message):
         response = "I'm feeling great! "
     else:
         response = "Not so good. "
-    response += "You are {} old {}. Have you considered more lotion for your face? How are you though?".format(temp, config["secret"]["city"])
+    response += "You are {} old. Have you considered more lotion for your face? How are you though?".format(temp, config["secret"]["city"])
 
     hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
 
