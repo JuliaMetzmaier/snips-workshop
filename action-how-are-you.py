e5c4c8faaf7e16bdf3538b95492e9033a0f018d0
JulesMitzy <julina.maier@googlemail.com>
2019-05-08 15:32:01 +0200

    changed responses

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -14,31 +14,30 @@ def main():
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
          .subscribe_intent(INTENT_GOOD, feeling_good_callback) \
          .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
-         .subscribe_intent(INTENT_ALRIGHT, feeling_alright_callback) \
          .start()
 
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "I'm doing great. How about you?"
+    response = "I'm still alive. How about you?"
     hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
 
 
 def feeling_good_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "That's awesome! I'm happy to hear that."
+    response = "That's awesome! Go kill yourself."
     hermes.publish_end_session(session_id, response)
 
 
 def feeling_bad_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "Sorry to hear that. I hope you feel better soon."
+    response = "Sorry to hear that. The bridge is only a few steps away."
     hermes.publish_end_session(session_id, response)
 
 
 def feeling_alright_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "That's cool."
+    response = "That's cool. See you in the afterlife."
     hermes.publish_end_session(session_id, response)
 
 
